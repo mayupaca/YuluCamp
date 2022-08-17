343ea2aa3d1ea1dee507a3f26d57c3358dce048f
Mayu Suzumura <mayumurasuzu@gmail.com>
2022-08-17 10:10:04 -0400

    created CRUD

--- app.js
+++ app.js
@@ -1,36 +1,78 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
-const Campground = require('./models/campground')
-
-mongoose.connect('mongodb://localhost:27017/yulu-camp',
-    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-    .then(() => {
-        console.log('Connected MongoDB Successfully!!');
-    })
-    .catch(err => {
-        console.log('Connected Error MongoDB!!');
-        console.log(err);
-    });
+const methodOverride = require("method-override");
+const Campground = require("./models/campground");
 
 const app = express();
 
 const port = 3000;
 
+mongoose
+  .connect("mongodb://localhost:27017/yulu-camp", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("Connected MongoDB Successfully!!");
+  })
+  .catch((err) => {
+    console.log("Connected Error MongoDB!!");
+    console.log(err);
+  });
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/makecampground", async (req, res) => {
-  const camp = new Campground({ title: "My camp site", description: 'Cozy camp' })
-  await camp.save();
-  res.send(camp)
+// --------------------------------------------------- All camp site
+app.get("/campgrounds", async (req, res) => {
+  // Campground modelの全データを.findメソッドで取得
+  const campgrounds = await Campground.find({});
+  res.render("campgrounds/index", { campgrounds });
 });
-
-
+// --------------------------------------------------- Create New Campsite Form
+app.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new");
+});
+// --------------------------------------------------- New formから送られた情報を受け取る
+app.post("/campgrounds", async (req, res) => {
+  const campground = await Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`campgrounds/${campground._id}`);
+});
+// --------------------------------------------------- Campsite detail
+app.get("/campgrounds/:id", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/show", { campground });
+});
+//  --------------------------------------------------- Edit
+app.get("/campgrounds/:id/edit", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/edit", { campground });
+});
+// ---------------------------------------------------- PUTで飛ばされた変更を受け取る
+app.put('/campgrounds/:id', async (req, res) => {
+  //更新したいcampsiteのIDを取得
+  const { id } = req.params;
+  // Campground Modelから対象のidのbody情報取得
+  const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+  res.redirect(`/campgrounds/${campground._id}`)
+});
+// ---------------------------------------------------- Delete
+app.delete('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params;
+  await Campground.findByIdAndDelete(id);
+  res.redirect('/campgrounds')
+})
+// ---------------------------------------------------- 
 app.listen(port, () => {
   console.log("Listening to port 3000...");
 });
--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const cities = require("./cities");
-const { descriptors, places } = require("./seedHelper");
+const { descriptors, places } = require("./seedHelpers");
 const Campground = require("../models/campground");
 
 mongoose
@@ -19,13 +19,14 @@ mongoose
 
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
+// データを新しくしたいときはターミナルから node seeds/index.js をして更新してあげないといけない。
 const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const randomCityIndex = Math.floor(Math.random() * cities.length);
     const camp = new Campground({
-      location: `${cities[randomCityIndex].prefecture}${cities[randomCityIndex].city}`,
-      title: `${sample(descriptors)}, ${sample(places)}`,
+      location: `${cities[randomCityIndex].city}, ${cities[randomCityIndex].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
     });
     await camp.save();
   }
