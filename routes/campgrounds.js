3a878e52d6d4457da49dc76204b69672e3cc8ab6
Mayu Suzumura <mayumurasuzu@gmail.com>
2022-09-15 07:58:37 -0400

    refactor routing and rating with star

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,20 +4,35 @@ const campgrounds = require("../controllers/campgrounds");
 const catchAsync = require("../utils/catchAsync");
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
 
-// --------------------------------------------------- All camp site
-router.get("/", catchAsync(campgrounds.index));
+
+router
+  .route("/")
+  // --------------------------------------------------- All camp site
+  .get(catchAsync(campgrounds.index))
+  // catchAsync関数(wrapper関数)をutilフォルダーに作って全てのasyncでerrorを使えるようにする
+  .post(
+    isLoggedIn,
+    validateCampground,
+    catchAsync(campgrounds.createCampground)
+  );
+
 // --------------------------------------------------- Create New Campsite Form
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
-// --------------------------------------------------- New formから送られた情報を受け取る
-// catchAsync関数(wrapper関数)をutilフォルダーに作って全てのasyncでerrorを使えるようにする
-router.post(
-  "/",
-  isLoggedIn,
-  validateCampground,
-  catchAsync(campgrounds.createCampground)
-);
-// --------------------------------------------------- Campsite detail
-router.get("/:id", catchAsync(campgrounds.showCampground));
+
+router
+  .route("/:id")
+  // --------------------------------------------------- Campsite detail
+  .get(catchAsync(campgrounds.showCampground))
+  // ---------------------------------------------------- PUTで飛ばされた変更を受け取る
+  .put(
+    isLoggedIn,
+    isAuthor,
+    validateCampground,
+    catchAsync(campgrounds.updateCampground)
+  )
+  // ---------------------------------------------------- Delete
+  .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
+
 //  --------------------------------------------------- Edit
 router.get(
   "/:id/edit",
@@ -25,20 +40,43 @@ router.get(
   isAuthor,
   catchAsync(campgrounds.renderEditForm)
 );
+
+//リファクター前
+// --------------------------------------------------- All camp site
+// router.get("/", catchAsync(campgrounds.index));
+// --------------------------------------------------- Create New Campsite Form
+// router.get("/new", isLoggedIn, campgrounds.renderNewForm);
+// --------------------------------------------------- New formから送られた情報を受け取る
+// catchAsync関数(wrapper関数)をutilフォルダーに作って全てのasyncでerrorを使えるようにする
+// router.post(
+//   "/",
+//   isLoggedIn,
+//   validateCampground,
+//   catchAsync(campgrounds.createCampground)
+// );
+// --------------------------------------------------- Campsite detail
+// router.get("/:id", catchAsync(campgrounds.showCampground));
+//  --------------------------------------------------- Edit
+// router.get(
+//   "/:id/edit",
+//   isLoggedIn,
+//   isAuthor,
+//   catchAsync(campgrounds.renderEditForm)
+// );
 // ---------------------------------------------------- PUTで飛ばされた変更を受け取る
-router.put(
-  "/:id",
-  isLoggedIn,
-  isAuthor,
-  validateCampground,
-  catchAsync(campgrounds.updateCampground)
-);
+// router.put(
+//   "/:id",
+//   isLoggedIn,
+//   isAuthor,
+//   validateCampground,
+//   catchAsync(campgrounds.updateCampground)
+// );
 // ---------------------------------------------------- Delete
-router.delete(
-  "/:id",
-  isLoggedIn,
-  isAuthor,
-  catchAsync(campgrounds.deleteCampground)
-);
+// router.delete(
+//   "/:id",
+//   isLoggedIn,
+//   isAuthor,
+//   catchAsync(campgrounds.deleteCampground)
+// );
 
 module.exports = router;
