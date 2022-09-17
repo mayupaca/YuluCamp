e0b01ae997d8060af72b26d5473e345efc8e1373
Mayu Suzumura <mayumurasuzu@gmail.com>
2022-09-17 17:24:31 -0400

    add cluster map to index

--- models/campground.js
+++ models/campground.js
@@ -12,33 +12,42 @@ ImageSchema.virtual("thumbnail").get(function () {
   return this.url.replace("/upload", "upload/w_200");
 });
 
-const campgroundSchema = new Schema({
-  title: String,
-  images: [ImageSchema],
-  geometry: {
-    type: {
-      type: String,
-      enum: ["Point"],
-      required: true,
+const opts = { toJSON: { virtuals: true } };
+const campgroundSchema = new Schema(
+  {
+    title: String,
+    images: [ImageSchema],
+    geometry: {
+      type: {
+        type: String,
+        enum: ["Point"],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
     },
-    coordinates: {
-      type: [Number],
-      required: true,
-    },
-  },
-  price: Number,
-  description: String,
-  location: String,
-  author: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-  reviews: [
-    {
+    price: Number,
+    description: String,
+    location: String,
+    author: {
       type: Schema.Types.ObjectId,
-      ref: "Review",
+      ref: "User",
     },
-  ],
+    reviews: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Review",
+      },
+    ],
+  },
+  opts
+);
+
+campgroundSchema.virtual("properties.popupMarkup").get(function () {
+  return `<strong><a href="/campgrounds/${this._id}">${this.title}</a></strong>
+  <p>${this.description.substring(0, 20)}...</p>`;
 });
 // ---------------------------------campgroundを削除したときにreviewも削除されるようにする
 // findOneAndDeleteはmongooseのミドルウェア
